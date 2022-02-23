8c1d11a6b38b34d825e962dc1cc8c302d8b8924a
niccellular <79813408+niccellular@users.noreply.github.com>
2022-02-23 09:44:02 -0500

    Update PluginPreferencesFragment.java
    License

--- app/src/main/java/com/atakmap/android/plugintemplate/PluginPreferencesFragment.java
+++ app/src/main/java/com/atakmap/android/plugintemplate/PluginPreferencesFragment.java
@@ -1,3 +1,10 @@
+/*
+Copyright (c) 2022, Nic Cellular
+All rights reserved.
+
+This source code is licensed under the BSD-style license found in the
+LICENSE file in the root directory of this source tree. package com.atakmap.android.plugintemplate;
+*/
 package com.atakmap.android.plugintemplate;
 
 
