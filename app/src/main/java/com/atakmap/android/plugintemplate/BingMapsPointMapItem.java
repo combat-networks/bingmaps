5e9e3190f98ba8e96b975ead16b4ae1186701801
niccellular <79813408+niccellular@users.noreply.github.com>
2022-02-23 09:43:09 -0500

    Update BingMapsPointMapItem.java
    License

--- app/src/main/java/com/atakmap/android/plugintemplate/BingMapsPointMapItem.java
+++ app/src/main/java/com/atakmap/android/plugintemplate/BingMapsPointMapItem.java
@@ -1,5 +1,10 @@
-package com.atakmap.android.plugintemplate;
+/*
+Copyright (c) 2022, Nic Cellular
+All rights reserved.
 
+This source code is licensed under the BSD-style license found in the
+LICENSE file in the root directory of this source tree. package com.atakmap.android.plugintemplate;
+*/
 import com.atakmap.android.maps.PointMapItem;
 import com.atakmap.coremap.maps.coords.GeoPoint;
 
